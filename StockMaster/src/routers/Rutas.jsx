81c080bdc91514aceb8a875cdde2406308887eba
Juan Manuel Agüero <juanmaguero4@gmail.com>
2024-01-11 21:29:20 -0300

    navbar

--- StockMaster/src/routers/Rutas.jsx
+++ StockMaster/src/routers/Rutas.jsx
@@ -1,18 +1,22 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
-import { Usuario } from '../components/usuarios/usuario'
 import { Sidebar } from '../components/headers/Sidebar'
+import { UsuariosPage } from '../page/UsuariosPage'
+
 
 
 export const Rutas = () => {
   return (
     <>
-     <div className="d-flex">
-   <Sidebar></Sidebar>  
-   <Routes>
-    <Route path="/usuario" element={<Usuario />}></Route>
-   </Routes>
-   </div>
+    
+      <div className="d-flex" >
+        <Sidebar />
+        <div className="content-container">
+          <Routes>
+            <Route path="/usuario" element={<UsuariosPage />}></Route>
+          </Routes>
+        </div>
+      </div>
     </>
   )
 }
